c59adf12dceb7f39787be6403b65908696e8d71e
Wilson Vargas <wilsonvargas_6@outlook.com>
2019-07-12 22:57:50 -0500

    initial commit

---
+++ src/XamarinSecureBackground/XamarinSecureBackground/Models/SecureCode.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinSecureBackground.Models
+{
+    public class SecureCode
+    {
+        public string Title { get; set; }
+        public string Code { get; set; }
+    }
+}
---
+++ src/XamarinSecureBackground/XamarinSecureBackground/ViewModels/MainViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using XamarinSecureBackground.Models;
+
+namespace XamarinSecureBackground.ViewModels
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<SecureCode> secureCodes;
+
+        public MainViewModel()
+        {
+            SecureCodes = new ObservableCollection<SecureCode>()
+            {
+                new SecureCode()
+                {
+                    Title = "Facebook",
+                    Code = "542366"
+                },
+                new SecureCode()
+                {
+                    Title = "Github",
+                    Code = "755335"
+                },
+                new SecureCode()
+                {
+                    Title = "Outlook",
+                    Code = "964344"
+                },
+                new SecureCode()
+                {
+                    Title = "Google",
+                    Code = "534223"
+                }
+            };
+        }      
+
+        public ObservableCollection<SecureCode> SecureCodes
+        {
+            get { return secureCodes; }
+            set
+            {
+                secureCodes = value;
+                RaisePropertyChanged("SecureCodes");
+            }
+        }
+               
+        private void RaisePropertyChanged(string propertyName)
+        {
+            var handle = PropertyChanged;
+            if (handle != null)
+                handle(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
